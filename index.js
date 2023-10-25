e388c21b96eb6a3cf27f95c1eaabf53d1c2da8cf
JeevalShah <114334723+JeevalShah@users.noreply.github.com>
2023-10-25 08:03:05 +0530

    Minor changes

--- index.js
+++ index.js
@@ -201,6 +201,19 @@ app.post("/appointment", async function(req, res) {
         rejectUnauthorized: false
       }
     });
+
+    await new Promise((resolve, reject) => {
+      // verify connection configuration
+      transporter.verify(function (error, success) {
+          if (error) {
+              console.log(error);
+              reject(error);
+          } else {
+              console.log("Server is ready to take our messages");
+              resolve(success);
+          }
+      });
+    });
     
     async function main() {
       const info = await transporter.sendMail({
